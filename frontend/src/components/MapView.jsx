abe0bdceabaa693937c89f0a77a78aa6684797b0
jessicajnx <jessica.jaunaux@gmail.com>
2025-04-15 12:45:03 +0200

    er

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -5,6 +5,8 @@ import L from 'leaflet';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import '../Form.css';
+
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
